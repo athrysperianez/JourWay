0888594e4ae9c193c751345b4bb7b74a81f42101
Elias Periañez <eliasperianez@gmail.com>
2018-10-02 18:38:17 +0200

    Completada estructura

---
+++ app/src/main/java/practicas/elias/jourway/LeisureActivity/LeisureActivity.java
@@ -0,0 +1,15 @@
+package practicas.elias.jourway.LeisureActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import practicas.elias.jourway.R;
+
+public class LeisureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leisure);
+    }
+}
